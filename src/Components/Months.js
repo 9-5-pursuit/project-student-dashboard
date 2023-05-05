e1bd44d0101215e2c5a50b80b332aac40782dbd0
davep80 <davidpaq1@gmail.com>
2023-05-05 18:28:07 -0400

    add cohorts to table rows

--- src/Components/Months.js
+++ src/Components/Months.js
@@ -1,24 +1,29 @@
 import React from 'react'
 import sData from '../data/data.json'
 
-function Months({setCards}) {
+function Months({ setCards }) {
     return (
         <>
-        <details>{JSON.stringify(sData)}</details>
-        <table className="table">
-            <thead>
-                <tr>
-                    <th>Choose a class by start date</th>
-                </tr>
-            </thead>
-            <tbody>
-            
-                <tr key={'all'}>
-                    <td style={{cursor: 'pointer'}}><a onClick={()=> setCards(sData)}>All Students</a></td>
-                    <td></td>
-                </tr>
-            </tbody>
-        </table>
+            <p>{ }</p>
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th>Choose a class by start date</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr key={0} className='border-success'>
+                        <td style={{ cursor: 'pointer' }}><a onClick={() => setCards(sData)}>All Students</a></td>
+                    </tr>
+                    {
+                        [...new Set(sData.map(item => item.cohort.cohortCode))].map((item, i) => {
+                            return <tr key={i+1} className='border-success'>
+                                <td style={{ cursor: 'pointer' }}><a onClick={() => setCards(sData)}>{item.replace(/(\D)(\d)/, '$1 $2')}</a></td>
+                            </tr>
+                        })
+                    }
+                </tbody>
+            </table>
         </>
     )
 }
