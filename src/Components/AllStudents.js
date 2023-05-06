0256c67404b0166dc04d75d9eaf3bbdcb9297546
davep80 <davidpaq1@gmail.com>
2023-05-06 00:45:28 -0400

    added all form functionality and prevent duplicate entry

--- src/Components/AllStudents.js
+++ src/Components/AllStudents.js
@@ -1,10 +1,26 @@
 import React from 'react'
 import { useState } from 'react'
 import { FaCheck } from 'react-icons/fa'
+import { v1 as generateId } from 'uuid'
 function AllStudents({ data }) {
 
-    const [index, setIndex] = useState(null)
+    const [index, setIndex] = useState(null);
+    const [Name, setName] = useState('');
+    const [Comment, setComment] = useState('');
+    const [id, setId] = useState('')
+    const [form, setForm] = useState({});
     var num = null
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!Name || !Comment) return
+        //store form data in state form object
+        //prevents duplicate Notes
+        setForm((prevState) => ({ ...prevState, [id]: [...new Set([...(prevState[id] || []), Name + ' says, ' + Comment])] }))
+        setName('')
+        setComment('')
+
+    };
 
     function makeDate(args) {
         const dateParts = args.split("/");
@@ -46,45 +62,72 @@ function AllStudents({ data }) {
                                     <h5 className="card-title">{Object.values(item['names']).join(' ')}</h5>
                                     <p className="card-text">{item['username']}</p>
                                     <p className="card-text"><span className='text-success'>Birthday:</span> {makeDate(item['dob'])}</p>
-                                    <a style={{ cursor: 'pointer' }} className="pointer" onClick={() => { handleCardExpand(i) }}>{index === i ? 'Show Less..' : 'Show More..'}</a>
+                                    <a style={{ cursor: 'pointer' }} className="pointer" onClick={() => { handleCardExpand(i); setId(item.id) }}>{index === i ? 'Show Less..' : 'Show More..'}</a>
                                 </div>
                             </div>
                             <div className="col-md-4 px-0">
                                 <div className='card-body'>
-                                <h5 className="card-title text-success">{status(item)}</h5>
+                                    <h5 className="card-title text-success">{status(item)}</h5>
                                 </div>
                             </div>
                         </div>
                         {index === i && (
-                            <div className="card m-1" id='sinfo' style={{ width: 'fit-content', borderColor: 'purple', borderWidth: 'thick' }}>
-                                <div className="card-body">
-                                    <div className="row">
-                                        <div className="col-sm-4">
-                                            <h6 className="card-subtitle py-1">Codewars:</h6>
-                                            <br />
-                                            <p><span className='text-success'>Current Total:</span> {item['codewars']['current']['total']}</p>
-                                            <p><span className='text-success'>Last Week:</span> {item['codewars']['current']['lastWeek']}</p>
-                                            <p><span className='text-success'>Goal:</span> {item['codewars']['goal']['total']}</p>
-                                            <p><span className={num >= 100 ? 'text-success' : (num >= 50 && num < 100) ? 'text-warning' : num < 50 ? 'text-danger' : '' }>Percent of Goal Achieved:</span> {num}%</p>
-                                        </div>
-                                        <div className="col-sm-4">
-                                            <h6 className="card-subtitle py-1">Scores</h6>
-                                            <br />
-                                            <p><span className='text-success'>Assignments:</span> {item['cohort']['scores']['assignments'] * 100}%</p>
-                                            <p><span className='text-success'>Projects:</span> {item['cohort']['scores']['projects'] * 100}%</p>
-                                            <p><span className='text-success'>Assessments:</span> {item['cohort']['scores']['assessments'] * 100}%</p>
-                                        </div>
-                                        <div className="col-sm-4">
-                                            <h6 className="card-subtitle py-1" id='cert'>Certifications</h6>
-                                            <br />
-                                            <p><span className='text-success'>Resume:</span> {item['certifications']['resume'] ? (<FaCheck />) : (<i className="fs-1 text-danger">x</i>)}</p>
-                                            <p><span className='text-success'>LinkedIn:</span> {item['certifications']['linkedin'] ? (<FaCheck />) : (<i className="fs-1 text-danger">x</i>)}</p>
-                                            <p><span className='text-success'>Mock Interview:</span> {item['certifications']['mockInterview'] ? (<FaCheck />) : (<i className="fs-1 text-danger">x</i>)}</p>
-                                            <p><span className='text-success'>GitHub:</span> {item['certifications']['github'] ? (<FaCheck />) : (<i className="fs-1 text-danger">x</i>)}</p>
+                            <>
+                                <div className="card m-1" id='sinfo' style={{ width: 'fit-content', borderColor: 'purple', borderWidth: 'thick' }}>
+                                    <div className="card-body">
+                                        <div className="row">
+                                            <div className="col-sm-4">
+                                                <h6 className="card-subtitle py-1">Codewars:</h6>
+                                                <br />
+                                                <p><span className='text-success'>Current Total:</span> {item['codewars']['current']['total']}</p>
+                                                <p><span className='text-success'>Last Week:</span> {item['codewars']['current']['lastWeek']}</p>
+                                                <p><span className='text-success'>Goal:</span> {item['codewars']['goal']['total']}</p>
+                                                <p><span className={num >= 100 ? 'text-success' : (num >= 50 && num < 100) ? 'text-warning' : num < 50 ? 'text-danger' : ''}>Percent of Goal Achieved:</span> {num}%</p>
+                                            </div>
+                                            <div className="col-sm-4">
+                                                <h6 className="card-subtitle py-1">Scores</h6>
+                                                <br />
+                                                <p><span className='text-success'>Assignments:</span> {item['cohort']['scores']['assignments'] * 100}%</p>
+                                                <p><span className='text-success'>Projects:</span> {item['cohort']['scores']['projects'] * 100}%</p>
+                                                <p><span className='text-success'>Assessments:</span> {item['cohort']['scores']['assessments'] * 100}%</p>
+                                            </div>
+                                            <div className="col-sm-4">
+                                                <h6 className="card-subtitle py-1" id='cert'>Certifications</h6>
+                                                <br />
+                                                <p><span className='text-success'>Resume:</span> {item['certifications']['resume'] ? (<FaCheck />) : (<i className="fs-1 text-danger">x</i>)}</p>
+                                                <p><span className='text-success'>LinkedIn:</span> {item['certifications']['linkedin'] ? (<FaCheck />) : (<i className="fs-1 text-danger">x</i>)}</p>
+                                                <p><span className='text-success'>Mock Interview:</span> {item['certifications']['mockInterview'] ? (<FaCheck />) : (<i className="fs-1 text-danger">x</i>)}</p>
+                                                <p><span className='text-success'>GitHub:</span> {item['certifications']['github'] ? (<FaCheck />) : (<i className="fs-1 text-danger">x</i>)}</p>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
+                                <div className="card m-2">
+                                    <div className="card-body">
+                                        <h5 className="card-title">1-on-1 Notes</h5>
+                                        <form onSubmit={(e) => handleSubmit(e)}>
+                                            <div className="mb-3">
+                                                <label htmlFor="cName" className="form-label">Commenter Name</label>
+                                                <input type="text" className="d-inline w-50 mx-2" id="cName" value={Name} onChange={(e) => setName(e.target.value)} />
+                                            </div>
+                                            <div className="mb-3">
+                                                <label htmlFor="comment" className="form-label">Comment</label>
+                                                <input type="text" className="d-inline w-50 mx-2" id="comment" value={Comment} onChange={(e) => setComment(e.target.value)} />
+                                            </div>
+                                            <button type="submit" className="btn btn-success">Add Note</button>
+                                        </form>
+                                    </div>
+                                    <ul>
+                                        {form[id] && (
+                                            form[id].map((item, i) => {
+                                                return <li key={generateId()}>
+                                                    {item}
+                                                </li>
+                                            })
+                                        )}
+                                    </ul>
+                                </div>
+                            </>
                         )}
                     </div>
 
