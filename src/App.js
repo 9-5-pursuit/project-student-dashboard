d8895fcf50ab57c80ba905b62506c2a84e47d1db
Irfan Hanjra <ihanjra@gmail.com>
2023-05-07 17:35:44 -0400

    Completed student list feature

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
+import studentData from "./data/data.json";
+import { useState } from "react";
+import StudentCard from "./Components/StudentCard";
 
 function App() {
+const [students, setStudents] = useState(studentData)
   return (
-    <div>
+    <div className="container">
+      
+      <header>
       <h1>Student Dashboard</h1>
+      </header>
+      
+      <main>
+        <div className="allStudents">
+          <h2>All Students</h2>
+          <p>Total Students: {studentData.length}</p>
+          {/* use map to render StudentCard for each student */}
+          {students.map((student) => (
+            <div className="studentCard" key={student.id}>
+              <StudentCard student={student} />
+            </div>
+          ))}
+        </div>
+      </main>
+    
     </div>
   );
 }
