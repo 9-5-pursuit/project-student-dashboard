a429641cdd41cd0eafe72de04ec48dd4a5df7efe
jeremytjimenez <jeremytjimenez@gmail.com>
2023-05-09 10:19:32 -0400

    made student total green

--- src/Components/StudentList.js
+++ src/Components/StudentList.js
@@ -4,7 +4,7 @@ export default function StudentList({ studentData, readableCohort }) {
     return (
         <div className="studentList">
             <h2>{readableCohort}</h2>
-            <p className='total'>Total Students: {studentData.length}</p>
+            <p className='total'>Total Students: <span>{studentData.length}</span></p>
             <ul>
                 {studentData.map((student) => {
                     return (
