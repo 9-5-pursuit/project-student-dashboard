8dda7a54b21123d3cbce247643c995492b64f457
zazimov <zalmanazimov@pursuit.org>
2023-05-09 02:37:54 -0400

    rendered page and all functions work

--- src/App.js
+++ src/App.js
@@ -1,8 +1,154 @@
+import studentData from "./data/data.json";
+import StudentCard from "./components/StudentCard";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [students, setStudents] = useState(studentData);
+  const [showCard, setShowCard] = useState(false);
+  const [cohortYear, setCohortYear] = useState("All Students");
+
+  function handleCohort(semester) {
+    // console.log(semester);
+    setStudents(
+      studentData.filter((item) => semester === item.cohort.cohortCode)
+    );
+
+    // console.log(semester);
+    const year = semester.slice(-4);
+    const season = semester.slice(0, -4);
+    console.log(season);
+    console.log(year);
+    setCohortYear(`${season} ${year}`);
+    // console.log(cohortYear);
+  }
+
   return (
-    <div>
-      <h1>Student Dashboard</h1>
+    <div className="App">
+      <h1 className="header text-center text-light">Student Dashboard</h1>
+      <div className="container">
+        <div className="row">
+          <div className="col-4 m-3">
+            <div className="col">
+              {/* <div className="row">
+                <span className="fw-bold">Choose a class by start date</span>
+              </div> */}
+              <table className="table my-2">
+                <thead>
+                  <tr>
+                    <th scope="col" className="fw-bold">
+                      Choose a class by start date
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr className="">
+                    <td scope="row">
+                      <a
+                        onClick={() => {
+                          setStudents(studentData);
+                          setCohortYear("All Students");
+                        }}
+                      >
+                        All Students
+                      </a>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td scope="row">
+                      <a onClick={() => handleCohort("Winter2026")}>
+                        Winter 2026
+                      </a>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td scope="row">
+                      <a onClick={() => handleCohort("Fall2026")}>Fall 2026</a>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td scope="row">
+                      <a onClick={() => handleCohort("Summer2026")}>
+                        Summer 2026
+                      </a>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td scope="row">
+                      <a onClick={() => handleCohort("Spring2026")}>
+                        Spring 2026
+                      </a>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td scope="row">
+                      <a onClick={() => handleCohort("Winter2025")}>
+                        Winter 2025
+                      </a>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td scope="row">
+                      <a onClick={() => handleCohort("Fall2025")}>Fall 2025</a>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td scope="row">
+                      <a onClick={() => handleCohort("Summer2025")}>
+                        Summer 2025
+                      </a>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td scope="row">
+                      <a onClick={() => handleCohort("Spring2025")}>
+                        Spring 2025
+                      </a>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+              {/* <hr /> */}
+              {/* <a href="#" className="row">
+                All Students
+              </a>
+              <hr className="border-primary" />
+              <a href="#" className="row">
+                Winter 2026
+              </a>
+              <a href="#" className="row">
+                Fall 2026
+              </a>
+              <a href="#" className="row">
+                Summer 2026
+              </a>
+              <a href="#" className="row">
+                Spring 2026
+              </a>
+              <a href="#" className="row">
+                Winter 2025
+              </a>
+              <a href="#" className="row">
+                Fall 2025
+              </a>
+              <a href="#" className="row">
+                Summer 2025
+              </a>
+              <a className="row">Spring 2025</a> */}
+            </div>
+          </div>
+          <div className="col-7 m-3">
+            <div className="m-3">
+              {<h3>{cohortYear}</h3>}
+              <p>
+                Total Students:{" "}
+                <span className="text-success">{students.length}</span>
+              </p>
+            </div>
+
+            <StudentCard students={students} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/StudentCard.js
@@ -0,0 +1,261 @@
+import { useState } from "react";
+import { FaCheck, FaTimes } from "react-icons/fa";
+
+export default function StudentCard({ students }) {
+  //   const [showCard, setShowCard] = useState(false);
+  const [inputName, setInputName] = useState("");
+  const [inputComment, setInputComment] = useState("");
+  const [indexState, setIndexState] = useState(null);
+  const [commStor, setCommStor] = useState({});
+
+  function formatBirthday(bday) {
+    //   const dateString = "2/3/1979";
+    const dateArray = bday.split("/"); // split the string into an array of parts
+    const year = dateArray[2];
+    const month = dateArray[0] - 1; // subtract 1 to convert to 0-based index
+    const day = dateArray[1];
+    const date = new Date(year, month, day); // create a new Date object
+    const formattedDate = date.toLocaleString("en-US", {
+      month: "long",
+      day: "numeric",
+      year: "numeric",
+    }); // format the date
+    return formattedDate; // output: "February 3, 1979"
+  }
+
+  function handleSubmit(e, person) {
+    e.preventDefault();
+
+    // {123xyz: ["commenter", "comment"],
+    // 456cbs: {},
+    // 789lma: {}}
+
+    const firstName =
+      inputName[0].toUpperCase() + inputName.slice(1).toLowerCase();
+
+    if (commStor[person.id]) {
+      setCommStor({
+        ...commStor,
+        [person.id]: [
+          ...commStor[person.id],
+          `${firstName} says, ${inputComment}`,
+        ],
+      });
+    } else {
+      setCommStor({
+        ...commStor,
+        [person.id]: [`${firstName} says, "${inputComment}"`],
+      });
+    }
+
+    setInputComment("");
+    setInputName("");
+  }
+
+  function handleOntrack(student) {
+    if (
+      student.certifications.resume &&
+      student.certifications.linkedin &&
+      student.certifications.github &&
+      student.certifications.mockInterview &&
+      student.codewars.current.total > 600
+    ) {
+      return <p className="text-success m-3">On Track to Graduate</p>;
+    }
+  }
+
+  function getPercentage(currentGoal, totalGoal) {
+    return Math.round((currentGoal / totalGoal) * 100);
+  }
+
+  function checkOrX(boolean) {
+    if (boolean) {
+      return <FaCheck color="green" />;
+    } else {
+      return <FaTimes color="red" />;
+    }
+    // console.log(boolean);
+  }
+
+  function colorChange(item) {
+    if (
+      getPercentage(item.codewars.current.total, item.codewars.goal.total) >=
+        0 &&
+      getPercentage(item.codewars.current.total, item.codewars.goal.total) < 50
+    ) {
+      return "text-danger";
+    } else if (
+      getPercentage(item.codewars.current.total, item.codewars.goal.total) >=
+        50 &&
+      getPercentage(item.codewars.current.total, item.codewars.goal.total) < 100
+    ) {
+      return "text-warning";
+    } else {
+      return "text-success";
+    }
+  }
+
+  return (
+    <>
+      {students.map((item, index) => {
+        return (
+          <div className="card m-3" key={item.id}>
+            <div className="row w-100">
+              <div className="col">
+                <img
+                  className="img-fluid rounded float-start p-3"
+                  style={{ minwidth: "100px" }}
+                  src={item.profilePhoto}
+                />
+              </div>
+              <div className="col">
+                <h6 className="pt-4">
+                  {item.names.preferredName} {item.names.middleName}{" "}
+                  {item.names.surname}
+                </h6>
+                <p>{item.username}</p>
+                <p>
+                  <span className="text-success">Birthday</span>:{" "}
+                  {formatBirthday(item.dob)}
+                </p>
+                {indexState !== index ? (
+                  <a
+                    className="text-success"
+                    onClick={() => {
+                      //   setShowCard(!showCard);
+                      setIndexState(index);
+                    }}
+                  >
+                    Show more...
+                  </a>
+                ) : (
+                  <a
+                    className="text-success"
+                    onClick={() => {
+                      //   setShowCard(!showCard);
+                      setIndexState(null);
+                    }}
+                  >
+                    Show less...
+                  </a>
+                )}
+              </div>
+              <div className="col">{handleOntrack(item)}</div>
+            </div>
+            {indexState === index && (
+              <div className="card">
+                <div className="row m-3">
+                  <div className="col">
+                    <h6>Codewars:</h6>
+                    <p>
+                      <span className="text-success">Current Total: </span>
+                      {item.codewars.current.total}
+                    </p>
+                    <p>
+                      <span className="text-success">Last Week: </span>
+                      {item.codewars.current.lastWeek}
+                    </p>
+                    <p>
+                      <span className="text-success">Goal: </span>
+                      {item.codewars.goal.total}
+                    </p>
+                    <p>
+                      <span className={colorChange(item)}>
+                        Percent of Goal Achieved:{" "}
+                      </span>
+                      {getPercentage(
+                        item.codewars.current.total,
+                        item.codewars.goal.total
+                      )}
+                      %
+                    </p>
+                  </div>
+                  <div className="col">
+                    <h6>Scores</h6>
+                    <p>
+                      <span className="text-success">Assignments: </span>
+                      {Math.round(item.cohort.scores.assignments * 100)}%
+                    </p>
+                    <p>
+                      <span className="text-success">Projects: </span>
+                      {Math.round(item.cohort.scores.projects * 100)}%
+                    </p>
+                    <p>
+                      <span className="text-success">Assessments: </span>
+                      {Math.round(item.cohort.scores.assessments * 100)}%
+                    </p>
+                  </div>
+                  <div className="col">
+                    <h6>Certifications</h6>
+                    <p>
+                      <span className="text-success">Resume: </span>
+                      {item.certifications.resume ? (
+                        <FaCheck color="green" />
+                      ) : (
+                        <FaTimes color="red" />
+                      )}
+                    </p>
+                    <p>
+                      <span className="text-success">LinkedIn: </span>
+                      {checkOrX(item.certifications.linkedin)}
+                    </p>
+                    <p>
+                      <span className="text-success">Mock Interview: </span>
+                      {checkOrX(item.certifications.mockInterview)}
+                    </p>
+                    <p>
+                      <span className="text-success">GitHub: </span>
+                      {checkOrX(item.certifications.github)}
+                    </p>
+                  </div>
+                </div>
+                <hr />
+                <div>
+                  <h4 className="m-3">1-on-1 Notes</h4>
+                  <form
+                    className="align-items-start p-3 m-3 border border-black"
+                    onSubmit={(event) => handleSubmit(event, item)}
+                  >
+                    <label htmlFor="commenter-name">Commenter Name</label>
+                    <input
+                      onChange={(event) => setInputName(event.target.value)}
+                      className="m-2"
+                      value={inputName}
+                      type="text"
+                      id="commenter-name"
+                      name="name"
+                    ></input>
+                    <p>
+                      <label htmlFor="commenter">Comment</label>
+                      <input
+                        onChange={(event) =>
+                          setInputComment(event.target.value)
+                        }
+                        className="m-2"
+                        value={inputComment}
+                        type="text"
+                        id="commenter-name"
+                        name="name"
+                      ></input>
+                    </p>
+
+                    <button type="submit" className="btn btn-success">
+                      Add Note
+                    </button>
+                  </form>
+                  {/* <p>{JSON.stringify(commStor)}</p> */}
+                  <ul>
+                    {commStor[item.id] &&
+                      commStor[item.id].map((entry, i) => {
+                        return <li key={item.id + i.toString()}>{entry}</li>;
+                      })}
+                  </ul>
+                </div>
+              </div>
+            )}
+          </div>
+        );
+      })}
+    </>
+  );
+}
